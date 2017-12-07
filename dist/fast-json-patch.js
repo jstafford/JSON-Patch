fee36f2bfe01c7a0b9c4387f48e890b08b29f2eb
Jason K Stafford <jason.stafford@scrycollective.com>
2017-12-07 12:05:46 -0800

    copy deepEqual into core to eliminate external dependencies

--- dist/fast-json-patch.js
+++ dist/fast-json-patch.js
@@ -67,7 +67,7 @@ var jsonpatch =
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
 /*!
  * https://github.com/Starcounter-Jack/JSON-Patch
@@ -75,11 +75,86 @@ var jsonpatch =
  * MIT license
  */
 
-const equalsOptions = { strict: true };
-const _equals = __webpack_require__(1);
-function areEquals(a, b) {
-  return _equals(a, b, equalsOptions);
-}
+function deepEqual (actual, expected) {
+   // 7.1. All identical values are equivalent, as determined by ===.
+   if (actual === expected) {
+     return true;
+
+   } else if (actual instanceof Date && expected instanceof Date) {
+     return actual.getTime() === expected.getTime();
+
+   // 7.3. Other pairs that do not both pass typeof value == 'object',
+   // equivalence is determined by ==.
+   } else if (!actual || !expected || typeof actual != 'object' && typeof expected != 'object') {
+     // return opts.strict ? actual === expected : actual == expected;
+     return actual === expected;
+
+   // 7.4. For all other Object pairs, including Array objects, equivalence is
+   // determined by having the same number of owned properties (as verified
+   // with Object.prototype.hasOwnProperty.call), the same set of keys
+   // (although not necessarily the same order), equivalent values for every
+   // corresponding key, and an identical 'prototype' property. Note: this
+   // accounts for both named and indexed properties on Arrays.
+   } else {
+     return objEquiv(actual, expected);
+   }
+ }
+
+ function isUndefinedOrNull(value) {
+   return value === null || value === undefined;
+ }
+
+ function isBuffer (x) {
+   if (!x || typeof x !== 'object' || typeof x.length !== 'number') return false;
+   if (typeof x.copy !== 'function' || typeof x.slice !== 'function') {
+     return false;
+   }
+   if (x.length > 0 && typeof x[0] !== 'number') return false;
+   return true;
+ }
+
+ function objEquiv(a, b, opts) {
+   var i, key;
+   if (isUndefinedOrNull(a) || isUndefinedOrNull(b))
+     return false;
+   // an identical 'prototype' property.
+   if (a.prototype !== b.prototype) return false;
+   if (isBuffer(a)) {
+     if (!isBuffer(b)) {
+       return false;
+     }
+     if (a.length !== b.length) return false;
+     for (i = 0; i < a.length; i++) {
+       if (a[i] !== b[i]) return false;
+     }
+     return true;
+   }
+   try {
+     var ka = Object.keys(a),
+         kb = Object.keys(b);
+   } catch (e) {//happens when one is a string literal and the other isn't
+     return false;
+   }
+   // having the same number of owned properties (keys incorporates
+   // hasOwnProperty)
+   if (ka.length != kb.length)
+     return false;
+   //the same set of keys (although not necessarily the same order),
+   ka.sort();
+   kb.sort();
+   //~~~cheap key test
+   for (i = ka.length - 1; i >= 0; i--) {
+     if (ka[i] != kb[i])
+       return false;
+   }
+   //equivalent values for every corresponding key, and
+   //~~~possibly expensive deep test
+   for (i = ka.length - 1; i >= 0; i--) {
+     key = ka[i];
+     if (!deepEqual(a[key], b[key], opts)) return false;
+   }
+   return typeof a === typeof b;
+ }
 
 /**
 * Deeply clone the object.
@@ -97,7 +172,6 @@ function deepClone(obj) {
             return obj; //no need to clone primitives
     }
 }
-exports.deepClone = deepClone;
 
 //3x faster than cached /^\d+$/.test(str)
 function isInteger(str) {
@@ -114,7 +188,6 @@ function isInteger(str) {
     }
     return true;
 }
-exports.isInteger = isInteger;
 
 /**
  * Unescapes a json pointer path
@@ -124,20 +197,6 @@ exports.isInteger = isInteger;
 function unescapePathComponent(path) {
     return path.replace(/~1/g, '/').replace(/~0/g, '~');
 }
-exports.unescapePathComponent = unescapePathComponent;
-
-function JsonPatchError(message, name, index, operation, tree) {
-  Error.call(this, message);
-  this.name = name;
-  this.index = index;
-  this.operation = operation;
-  this.tree = tree;
-}
-
-JsonPatchError.prototype = Object.create(Error.prototype);
-JsonPatchError.prototype.constructor = JsonPatchError;
-
-exports.JsonPatchError = JsonPatchError;
 
 /* We use a Javascript hash to store each
  function. Each hash entry (property) uses
@@ -191,7 +250,7 @@ const objOps = {
     return { newDocument: document };
   },
   test: function (obj, key, document) {
-    return { newDocument: document, test: areEquals(obj[key], this.value) };
+    return { newDocument: document, test: deepEqual(obj[key], this.value) };
   },
   _get: function (obj, key, document) {
     this.value = obj[key];
@@ -272,9 +331,9 @@ function applyOperation(document, operation) {
       }
       return returnValue;
     } else if (operation.op === 'test') {
-      returnValue.test = areEquals(document, operation.value);
+      returnValue.test = deepEqual(document, operation.value);
       if (returnValue.test === false) {
-        throw new JsonPatchError("Test operation failed", 'TEST_OPERATION_FAILED', 0, operation, document);
+        throw new Error("Test operation failed");
       }
       returnValue.newDocument = document;
       return returnValue;
@@ -328,7 +387,7 @@ function applyOperation(document, operation) {
     }
 
     if (returnValue.test === false) {
-      throw new JsonPatchError("Test operation failed", 'TEST_OPERATION_FAILED', 0, operation, document);
+      throw new Error("Test operation failed");
     }
     return returnValue;
   }
@@ -357,147 +416,6 @@ function applyPatch(document, patch) {
   return results;
 }
 exports.applyPatch = applyPatch;
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-var pSlice = Array.prototype.slice;
-var objectKeys = __webpack_require__(2);
-var isArguments = __webpack_require__(3);
-
-var deepEqual = module.exports = function (actual, expected, opts) {
-  if (!opts) opts = {};
-  // 7.1. All identical values are equivalent, as determined by ===.
-  if (actual === expected) {
-    return true;
-
-  } else if (actual instanceof Date && expected instanceof Date) {
-    return actual.getTime() === expected.getTime();
-
-  // 7.3. Other pairs that do not both pass typeof value == 'object',
-  // equivalence is determined by ==.
-  } else if (!actual || !expected || typeof actual != 'object' && typeof expected != 'object') {
-    return opts.strict ? actual === expected : actual == expected;
-
-  // 7.4. For all other Object pairs, including Array objects, equivalence is
-  // determined by having the same number of owned properties (as verified
-  // with Object.prototype.hasOwnProperty.call), the same set of keys
-  // (although not necessarily the same order), equivalent values for every
-  // corresponding key, and an identical 'prototype' property. Note: this
-  // accounts for both named and indexed properties on Arrays.
-  } else {
-    return objEquiv(actual, expected, opts);
-  }
-}
-
-function isUndefinedOrNull(value) {
-  return value === null || value === undefined;
-}
-
-function isBuffer (x) {
-  if (!x || typeof x !== 'object' || typeof x.length !== 'number') return false;
-  if (typeof x.copy !== 'function' || typeof x.slice !== 'function') {
-    return false;
-  }
-  if (x.length > 0 && typeof x[0] !== 'number') return false;
-  return true;
-}
-
-function objEquiv(a, b, opts) {
-  var i, key;
-  if (isUndefinedOrNull(a) || isUndefinedOrNull(b))
-    return false;
-  // an identical 'prototype' property.
-  if (a.prototype !== b.prototype) return false;
-  //~~~I've managed to break Object.keys through screwy arguments passing.
-  //   Converting to array solves the problem.
-  if (isArguments(a)) {
-    if (!isArguments(b)) {
-      return false;
-    }
-    a = pSlice.call(a);
-    b = pSlice.call(b);
-    return deepEqual(a, b, opts);
-  }
-  if (isBuffer(a)) {
-    if (!isBuffer(b)) {
-      return false;
-    }
-    if (a.length !== b.length) return false;
-    for (i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  }
-  try {
-    var ka = objectKeys(a),
-        kb = objectKeys(b);
-  } catch (e) {//happens when one is a string literal and the other isn't
-    return false;
-  }
-  // having the same number of owned properties (keys incorporates
-  // hasOwnProperty)
-  if (ka.length != kb.length)
-    return false;
-  //the same set of keys (although not necessarily the same order),
-  ka.sort();
-  kb.sort();
-  //~~~cheap key test
-  for (i = ka.length - 1; i >= 0; i--) {
-    if (ka[i] != kb[i])
-      return false;
-  }
-  //equivalent values for every corresponding key, and
-  //~~~possibly expensive deep test
-  for (i = ka.length - 1; i >= 0; i--) {
-    key = ka[i];
-    if (!deepEqual(a[key], b[key], opts)) return false;
-  }
-  return typeof a === typeof b;
-}
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-exports = module.exports = typeof Object.keys === 'function'
-  ? Object.keys : shim;
-
-exports.shim = shim;
-function shim (obj) {
-  var keys = [];
-  for (var key in obj) keys.push(key);
-  return keys;
-}
-
-
-/***/ }),
-/* 3 */
-/***/ (function(module, exports) {
-
-var supportsArgumentsClass = (function(){
-  return Object.prototype.toString.call(arguments)
-})() == '[object Arguments]';
-
-exports = module.exports = supportsArgumentsClass ? supported : unsupported;
-
-exports.supported = supported;
-function supported(object) {
-  return Object.prototype.toString.call(object) == '[object Arguments]';
-};
-
-exports.unsupported = unsupported;
-function unsupported(object){
-  return object &&
-    typeof object == 'object' &&
-    typeof object.length == 'number' &&
-    Object.prototype.hasOwnProperty.call(object, 'callee') &&
-    !Object.prototype.propertyIsEnumerable.call(object, 'callee') ||
-    false;
-};
 
 
 /***/ })
